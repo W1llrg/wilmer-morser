4ef1801b9460469f2b23bf086d199b6ecc946544
Willrg <will.ragueneau@gmail.com>
2023-12-18 17:16:56 +0100

    🐛 FIX: changed about

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -6,7 +6,7 @@ module.exports = {
         .setName('about')
         .setDescription('Replies with cool infos'),
     async execute(interaction) {
-        msg = '```.... .. / .. - ... / .-- .. .-.. -- . .-. / -- --- .-. ... . .-.```';
+        msg = '```A5x87fEKD6Ua9/N5R6+0jB4KlWmYDF92NgShA/+/qCJoJd16sK2vua5JL7WIfrWO9q7s0gTpaCcjjmWPW7IHjOMwTeOFCyGTQg1I5FE/Z8YfJneiBpE9A5dI3W13vf12g5zw51opK9c=```';
         await interaction.reply(msg);
     },
 };
