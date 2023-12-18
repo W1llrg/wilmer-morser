c2ba3b805f10e4e55b32f2131fecacd7b7acdf28
Willrg <will.ragueneau@gmail.com>
2023-12-18 16:26:03 +0100

    👌 IMPROVE: moved logic and added morse alphabet

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder()
         .setName('about')
         .setDescription('Replies with cool infos'),
